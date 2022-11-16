6fa0f9061f61fe47559efbaed12acdf331ee08c7
Aimeric Marchau <marchauaimeric@gmail.com>
2022-11-16 13:18:24 +0100

    add cloudinary and lists cover images

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,6 +24,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :image)
+    params.require(:list).permit(:name, :cover_image)
   end
 end
